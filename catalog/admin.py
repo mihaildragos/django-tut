f633ef56aac6cdfbf798d827fb21335b381fa0d0
Mihail <museamihai@gmail.com>
2019-08-20 15:29:26 +0100

    initial commit with a barebones website

---
+++ catalog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from catalog.models import Author, Genre, Book, BookInstance, Language
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Language)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+
