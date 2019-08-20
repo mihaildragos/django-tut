88bbc289f60b6c4f5e06d8316e85eaa783390e13
Mihail <museamihai@gmail.com>
2019-08-20 15:57:09 +0100

    added some admin models to display data with more details in admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from catalog.models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(Author)
 admin.site.register(Language)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+# admin.site.register(BookInstance)
 
+# We can register models for how the information is displayed in the admin panel by adding models and registering them with admin.site.register() syntax
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+admin.site.register(Author, AuthorAdmin)
+
+# Or we can add a decorator @register which does the same thing as admin.site.register()
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+@admin.register(BookInstance) 
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
--- catalog/models.py
+++ catalog/models.py
@@ -49,6 +49,14 @@ class Book(models.Model):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
 
+    def display_genre(self):
+    	pass
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
 
 
 class BookInstance(models.Model):
@@ -64,7 +72,7 @@ class BookInstance(models.Model):
         ('a', 'Available'),
         ('r', 'Reserved'),
     )
-
+    
     status = models.CharField(
         max_length=1,
         choices=LOAN_STATUS,
